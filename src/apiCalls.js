8d4574293082e9b601d56605ffe729188cad7d32
Hollis Vohr <hollis.vohr@gmail.com>
2023-06-10 14:43:52 -0600

    feat: add post skeleton to apiCalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -26,9 +26,29 @@ const getAllBookings = () => {
     .catch((error) => alert(error))
 }
 
+const addPostBooking = (postBooking, currentUser) => {
+  fetch('http://localhost:3001/api/v1/bookings', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(newSavedBooking(postBooking, currentUser))
+  })
+  .then(response => response.json())
+  .catch(error => alert(error))
+}
+
+const newSavedBooking = (bookingInfo, currentUser) => { 
+  return {
+    userID: currentUser.id, 
+    date: bookingInfo.date,
+    roomNumber: bookingInfo.number 
+  }
+}
+
+
 export {
   getAllCustomers,
   getSingleCustomer,
   getAllRooms,
-  getAllBookings
+  getAllBookings,
+  addPostBooking
 }
