c53fcf3989a9a249d206a9decf5b26efc36a6df5
Hollis Vohr <hollis.vohr@gmail.com>
2023-06-11 19:36:10 -0600

    fix: repair test suite error with total cost function

--- test/customerUtils-test.js
+++ test/customerUtils-test.js
@@ -2,6 +2,7 @@ import chai from 'chai';
 const expect = chai.expect;
 import { bookingsData } from '/Users/hollisvohr/turing_work/mod_2/outlook-project/src/data/bookings-sample.js';
 import { displayPrevBookedRooms, findValidIDNumber, calculateBookingCost } from '../src/customerUtils.js' 
+import { roomsData } from '/Users/hollisvohr/turing_work/mod_2/outlook-project/src/data/rooms-sample-data.js';
 import { customerData } from '/Users/hollisvohr/turing_work/mod_2/outlook-project/src/data/customers-sample-data.js';
 
 describe('displayPrevBookedRooms', function() {
@@ -16,7 +17,8 @@ describe('displayPrevBookedRooms', function() {
     expect(prevBooked).to.deep.equal(expected)
   })
   it('should display no booking if none made by that user', () => {
-    let prevBooked = displayPrevBookedRooms(24, bookingsData)
+    let customerObject = {"id":24,"name":"Hollis Vohr"}
+    let prevBooked = displayPrevBookedRooms(customerObject, bookingsData)
     let expected = []
     expect(prevBooked.length).to.equal(0)
     expect(prevBooked).to.deep.equal(expected)
@@ -42,15 +44,14 @@ describe('calculateBookingCost', function() {
     expect(calculateBookingCost).to.be.a('function')
   });
   it('should return the booking cost for one scheduled room', () => {
-    let room1Cost = 358.4
-    let totalCost = calculateBookingCost(roomsData,[room1Cost])
-    let expected = 358.4
+    let prevBooked = displayPrevBookedRooms({"id":1,"name":"Leatha Ullrich"}, bookingsData)
+    let totalCost = calculateBookingCost(prevBooked, roomsData)
+    let expected = 397.02
     expect(totalCost).to.deep.equal(expected)
   })
   it('should return the booking cost for multiple scheduled rooms', () => {
-    let room1Cost = 358.4
-    let room2Cost = 429.44
-    let totalCost = calculateBookingCost(roomsData,[room1Cost, room2Cost])
-    expect(totalCost).to.equal(room1Cost + room2Cost)
+    let prevBooked = displayPrevBookedRooms({"id":9,"name":"Faustino Quitzon"}, bookingsData)
+    let totalCost = calculateBookingCost(prevBooked, roomsData)
+    expect(totalCost).to.equal(691.5799999999999)
   })
 })
